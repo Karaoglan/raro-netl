c27efc1c2831e16f8590a5f84eb08c3a55722ed9
Karaoglan <burak.karaoglan>
2022-04-15 23:44:04 +0300

    specify node version 7

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-import "./assets/css/plugins/bootstrap.min.css";
-import "remixicon/fonts/remixicon.css";
-import "./assets/scss/style.scss";
-import { Web3ReactProvider } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-import Router from "./Router/routes";
-function App() {
-    const getLibrary = (provider) => {
-        const library = new Web3Provider(provider, "any");
-        library.pollingInterval = 15000;
-        return library;
-    };
-
-    return (
-        <Web3ReactProvider getLibrary={getLibrary}>
-            <div className="App overflow-hidden">
-                <Router />
-            </div>
-        </Web3ReactProvider>
-    );
-}
-
-export default App;
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+);
